fb6d55b90fc2ce20074ffe56c94e09de07582b8d
MGS\norbert.kedves <norbert.kedves@magicsolutions.ro>
2021-02-18 16:46:14 +0200

    tanulo kartya web inditasa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
+
+
+function Card() {
+  return <div className="flash-card">Orszag neve</div>;
+    
+}
+
+
+export default function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> MemoCards</h1>
+      <h2> Kattints a kártyára az ország fővárosának megtekintéséhez</h2>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
-
-export default App;
