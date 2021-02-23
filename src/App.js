d311b3c4caca89fdc25dbf03c83969ceeaa43c63
Norbert Kedves <norbert.kedves@magicsolutions.ro>
2021-02-23 20:30:32 +0200

    map

--- src/App.js
+++ src/App.js
@@ -1,55 +1,42 @@
 import React from 'react';
 import Card from "./cards";
+import ReactDOM from "react-dom";
+
+
+const persons =[
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"},
+    {firstname : "Jayne", lastname: "Cobb"},
+    {firstname : "Jayne", lastname: "Cobb"},
+    {firstname : "Jayne", lastname: "Cobb"},
+    {firstname : "Jayne", lastname: "Cobb"},
+    {firstname : "Jayne", lastname: "Cobb"}
+];
+
+
 
 
 export default function App(){
+
   return (
+    <React.Fragment>
     <div className="App">
       <h1> MemoCards</h1>
       <h2> Kattints a kártyára az ország fővárosának megtekintéséhez</h2>
-      <section className="cards">
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card orszag = "Romania" fovaros = "Bukarest" />    
-        </article>
-
-        <article class="card">
-          <Card orszag = "Magyaroszag" fovaros = "Budapest" />    
-        </article>
-
-        <article class="card">
-          <Card  orszag = "Nemetorszag" fovaros = "Berlin" />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-
-        <article class="card">
-          <Card />    
-        </article>
-      </section>
-        
-      
     </div>
+    <div className="cardContainer">
+      <div className="cards">
+        {persons.map((item, index) => {
+          return (
+          <div class="card"> 
+            <div className="text">{item.firstname}</div>
+          </div>
+      );
+      })}
+        </div>
+      </div>
+      </React.Fragment>
+
   );
 }
